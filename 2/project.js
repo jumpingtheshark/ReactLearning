3655ba4f10465f7b2bae31ba4c04b8d9cefdd3ab
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-09-30 12:50:49 -0500

    Add files via upload
    the project.js has the data, and it's supposed to be in a separate folder, but I'm just tossing em all in the right now into the same one... I'll have to have the seychel to hcange the "import" or whatever.

---
+++ 2/project.js
@@ -0,0 +1,22 @@
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Example React Application',
+        description: 'A React App that I built, involving JS and core web dev concepts!',
+        link: 'https://github.com/15Dkatz/example'
+    },
+    {
+        id: 2,
+        title: 'My API',
+        description: 'A REST API that I built from scratch with GET and POST requests!',
+        link: 'https://github.com/15Dkatz/example'
+    },
+    {
+        id: 3,
+        title: 'Operating Systems Final Project',
+        description: 'My unique final project for my university Operating Systems course.',
+        link: 'https://github.com/15Dkatz/example'
+    }
+];
+
+export default PROJECTS;
