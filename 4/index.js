3db4f78264da49d262831f6b8b5e92d90f23033c
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-10-04 12:41:49 -0500

    Create index.js

---
+++ 4/index.js
@@ -0,0 +1,8 @@
+//import ReactDOM from 'react-dom'
+
+console.log ('hi')
+
+
+const element=React.createElement(`div`, null, 'React element');
+
+ReactDOM.render(element, document.getElementById('root'));
