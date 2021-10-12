1dad00fc5792789953d387567ab0de6d6add2bc6
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-10-12 12:07:19 -0500

    Update Header.js

--- 7/Header.js
+++ 7/Header.js
@@ -4,10 +4,17 @@ import {Link } from 'react-router-dom'
 
 const   Header = () =>
 {
+
+ const style ={
+     display: 'inline-block',
+     margin: 10,
+     marginBottom: 30
+
+ };
     return (
         <div>
-            <h3> <Link to = '/'>Home </Link></h3>
-            <h3> <Link to = '/jokes'>Jokes </Link></h3>
+            <h3 style={style}> <Link to = '/'>Home </Link></h3>
+            <h3 style={style}> <Link to = '/jokes'>Jokes </Link></h3>
         </div>
 
 
