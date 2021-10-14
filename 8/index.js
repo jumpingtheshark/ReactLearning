bcb3de976011409222f465b621c2c45f9c11825c
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-10-14 12:24:42 -0500

    Create index.js

---
+++ 8/index.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
+
+console.log("about to fetch")
+let bored = "http://www.boredapi.com/api/activity"
+let mike = "http://localhost:5001/mike"
+const data = { username: 'example' };
+
+
+function foo1() {
+    fetch(mike)
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(response => console.log("problem!!!"))
+}
+
+function foo2() {
+    fetch(mike, {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+}
+
+
+
+
+
+
