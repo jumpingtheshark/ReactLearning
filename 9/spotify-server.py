1fae58a9fb35a7f7db65a5483df083f8ce052c82
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-10-29 12:23:09 -0500

    Update spotify-server.py

--- 9/spotify-server.py
+++ 9/spotify-server.py
@@ -1,69 +1,142 @@
-#https://flask.palletsprojects.com/en/2.0.x/quickstart/
-import flask
-from flask_cors import CORS
-from flask import Flask, request, jsonify, session
-from datetime import date
-import requests
-import logging
+import './App.css';
+import React, {Component} from "react";
+// npm start
 
 
-app = Flask(__name__)
-CORS(app)
 
-@app.route("/",  methods=['GET','POST'])
-def hello_world():
-   # data=request.get_json(force=True)
-    return " blah"
+class App extends Component {
+    constructor() {
+        super();
 
-
-#app.get('/artist/:name', (req, res, next) => {
-
-#curl -X "GET" "https://api.spotify.com/v1/search?q=Bob%20Dylan&type=artist"
-# -H "Accept: application/json" -H "Content-Type: application/json"
-# -H "Authorization: Bearer BQAFa0S6nHoW"
-
-# sample call http://172.31.89.85:5001/Artist?name=mick%20jagger
-# spotify api call: https://developer.spotify.com/console/get-search-item/?q=Bob%20Dylan&type=artist&market=&limit=&offset=&include_external=
-
-@app.route("/Artist",  methods=['GET','POST'])
-def get_artist():
-    client_id = "3032d7618ce34dffaf349214bba92cd5"
-    client_secret = "a6bf34e7fc4b4066991b3d344f465a3f"
-
-    #BASE_URL = 'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V'
-    #BASE_URL = 'https://api.spotify.com/v1/artists/6hKLYGlOtGVNzZUuvS99Nw/top-tracks?market=ES'  # vysotsky
-    BASE_URL = 'https://api.spotify.com/v1/search?q={name}&type=artist'
+        this.state = {
+            artistQuery: '',
+            artistName: '',
+            followers: '',
+            imageUrl:'',
+            spotifyID:'',
+            queryResult: {}
 
 
-    # scope = "appstore::apps:readwrite"
-    grant_type = "client_credentials"
-    data = {
-        "grant_type": grant_type,
-        "client_id": client_id,
-        "client_secret": client_secret,
+            // this.foo2 = this.foo2.bind(this)
+        }
 
     }
-    # auth_url = "https://api.amazon.com/auth/o2/token"
-    auth_url = "https://accounts.spotify.com/api/token"
+updateArtistQuery = event=> {
+console.log ('event', event.target.value);
+this.setState({artistQuery: event.target.value})
 
-    auth_response = requests.post(auth_url, data=data)
+}
 
-    # Read token from auth response
-    auth_response_json = auth_response.json()
-    auth_token = auth_response_json["access_token"]
+searchArtist = () => {
 
-    auth_token_header_value = "Bearer %s" % auth_token
+    //http://172.31.89.85:5001/Artist?name=mick%20jagger
+    //console.log ("button clicked")
 
-    #auth_token_header = {"Authorization": auth_token_header_value}
-    #print(auth_token)
-
-    artist_name = request.args.get('name')
-    BASE_URL=BASE_URL.format(name=artist_name)
-    r = requests.get(BASE_URL, headers={'Authorization': 'Bearer ' + auth_token})
-    rjson = r.json()
-    return jsonify(rjson)
+    /*
+         fetch ("http://www.boredapi.com/api/activity/")
+            .then (response=>response.json())
+            .then(json=>this.foo2(json))
+         */
 
 
+    console.log ("searchArtist() artist query",  this.state.artistQuery)
+    let name = this.state.artistQuery
+    //let GET_ARTIST_URL =  "http://172.31.89.85:5001/Artist?name="+name
+    let GET_ARTIST_URL =  "http://localhost:5001/Artist?name="+name
+    console.log(GET_ARTIST_URL)
+    fetch (GET_ARTIST_URL)
+        // .then (response=>this.foo2(response))
+        .then (response=>response.json())
+        //.then (json=>console.log(json))
+        .then (json=>this.foo2(json))
+        //.then (json=>this.setState({queryResult:json}))
 
 
-app.run(host='0.0.0.0',port=5001)
+}
+
+    foo2(artist) {
+
+       console.log(artist)
+
+
+/*
+        for (let key in artists){
+            console.log(artists[key])
+           // console.log ( key.name)
+
+        }
+*/
+        //this.setState({queryResult:artists})
+        //this.setState({queryResult:['a', 'b', 'c', 'd', 'e', 'f','g']})
+
+        //let info =this.state.queryResult
+        //{"image_url":"","name":"","spotify_id":"","total_followers":56146}
+        const {image_url, name, spotify_id, total_followers}=artist
+       // artistName: '',
+        this.setState({artistName:name})
+        //    followers: '',
+        this.setState ({followers:"Followers: " + total_followers})
+        //    url:'',
+        //    queryResult: {}
+
+
+        this.setState({queryResult:artist})
+        this.setState({imageUrl:image_url})
+
+
+
+        //return undefined;
+    }
+
+  
+/*
+handleKeyPress = event => {
+
+   // this.setState({artistName:event.target.value}) // useless for now
+    this.setState({artistQuery: event.target.value})
+    if (event.key=='Enter'){
+
+        console.log ('event', event.target.value);
+       // this.setState({artistQuery: event.target.value})
+      //  this.searchArtist()
+
+
+    }
+}
+*/
+  render(){
+        return (
+            <div className="App">
+                <h2>Music Master</h2>
+              <input onChange={this.updateArtistQuery}
+              placeholder='Search for an artist'
+              />
+
+                <button onClick={this.searchArtist}>Search</button>
+
+
+                <div>
+
+
+
+<p>
+                    {this.state.artistName}
+</p>
+                    <p>
+                   {this.state.followers}
+                    </p>
+
+                    <image src={imageUrl}>
+
+                    </image>
+
+
+                </div>
+
+
+            </div>
+        );
+    }
+}
+export default App;
+
