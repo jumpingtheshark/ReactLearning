ffbf9ce3573f9ecada22baa1d4b0a586f112efc8
Michael Rubinstein <mike.rubinstein@gmail.com>
2021-11-01 13:43:47 -0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,31 @@
+export class Arrays {
+
+     foo() {
+        let a = []
+        a.push('a', 'b', 'c')
+
+        a.map(b => {
+            console.log(b)
+        })
+    }
+
+
+
+
+
+
+foo2() {
+    let v = []
+    v.push('a', 'b', 'c')
+
+        for (let i=0; i<v.length; i++){
+            console.log(v[i])
+        }
+     }
+
+
+}
+
+
+export default Arrays
+
